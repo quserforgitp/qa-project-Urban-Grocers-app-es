1fc35a7bd32ea3005c30794711d2a60786ae437d
Helios <devbaru@gmail.com>
2024-12-17 14:22:41 -0600

    updated create_kit_name_kit_test. Codigo para prueba 3

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -44,8 +44,18 @@ def positive_assert(kit_name):
 # Funcion para pruebas negativas
 def negative_assert(kit_name):
     kit_body = get_create_kit_body(kit_name)
-    #TODO codigo para pruebas negativas
-    pass
+    # El token recibido del usuario creado se guarda en la variable token
+    token = post_new_user_and_extract_its_token()
+    # Los headers con los datos necesarios actualizados para crear un kit para un usuario se guarda en la variable kit_headers
+    kit_headers = get_create_kit_headers(token)
+    # El body actualizado para crear un kit para un usuario se guarda en la variable kit_body
+    kit_body = get_create_kit_body(kit_name)
+
+    # El resultado de la solicitud para crear un nuevo kit para un/a usuario/a se guarda en la variable kit_response
+    kit_response = sender_stand_request.post_new_kit_for_user(kit_headers, kit_body)
+
+    # Comprueba si el codigo de la respuesta es 400
+    assert kit_response.status_code == 400
 
 # TESTS
 # Prueba 1. Creación de un nuevo kit dentro de un usuario/a
@@ -57,3 +67,8 @@ def test_1_create_kit_for_user_1_letter_in_name_get_success_response():
 # El parámetro "name" contiene 511 caracteres
 def test_2_create_kit_for_user_511_letters_in_name_get_success_response():
     positive_assert(data.VALID_STRING_WITH_511_LETTERS)
+
+# Prueba 3. Creación de un nuevo kit dentro de un usuario/a
+# El parámetro "name" contiene 0 caracteres
+def test_3_create_kit_for_user_0_letters_in_name_get_error_response():
+    negative_assert("")
